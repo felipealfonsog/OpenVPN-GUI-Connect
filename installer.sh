e9713195e7e547d1367e4e10855d8ccb2e399935
Fellipe Alfonso González <f.alfonso@res-ear.ch>
2024-08-17 06:45:24 -0400

    Updates

--- installer.sh
+++ installer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 # Define variables
@@ -7,103 +6,144 @@ PKG_NAME="ovpnconn"
 PKG_VER="0.0.1"
 REPO_URL="https://github.com/felipealfonsog/OpenVPN-GUI-Connect"
 ARCHIVE_URL="${REPO_URL}/archive/refs/tags/v.${PKG_VER}.tar.gz"
-SRC_DIR="${PKG_NAME}-${PKG_VER}"
+SRC_DIR="OpenVPN-GUI-Connect-v.${PKG_VER}"
 INSTALL_DIR="/usr/local/bin"
-ICON_DIR="/usr/share/pixmaps"
+ICON_DIR="/usr/local/share/icons"
 DESKTOP_DIR="/usr/share/applications"
 
-# Welcome message function
 welcome() {
     echo "
-    ╔═════════════════════════════════════════════════════════╗
-    ║                                                         ║
-    ║   ~ OpenVPN-GUI-Connect Installer ~                     ║
-    ║   Developed with ❤️ by Felipe Alfonso González L.       ║
-    ║   Computer Science Engineer                             ║
-    ║   Chile                                                 ║
-    ║                                                         ║
-    ║   Contact: f.alfonso@res-ear.ch                         ║
-    ║   Licensed under BSD 3-clause                           ║
-    ║   GitHub: github.com/felipealfonsog                     ║
-    ║                                                         ║
-    ╚═════════════════════════════════════════════════════════╝
+    ╔═══════════════════════════════════════╗
+    ║                                       ║
+    ║   ~ OpenVPN-GUI-Connect ~             ║
+    ║   Developed with ❤️ by                ║
+    ║   Felipe Alfonso González L.          ║
+    ║   Computer Science Engineer           ║
+    ║   Chile                               ║
+    ║                                       ║
+    ║   Contact: f.alfonso@res-ear.ch       ║
+    ║   Licensed under BSD 3-clause         ║
+    ║   GitHub: github.com/felipealfonsog   ║
+    ║                                       ║
+    ╚═══════════════════════════════════════╝
     "
     echo "Welcome to the OpenVPN-GUI-Connect Installer!"
-    echo "---------------------------------------------------------------------"
+    echo "--------------------------------------------"
 }
 
-# Function to install dependencies
-install_dependencies() {
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    if command -v pacman &> /dev/null; then
-      sudo pacman -Syu --needed python openvpn python-pyqt5 pkexec curl
-    elif command -v apt &> /dev/null; then
-      sudo apt update && sudo apt install -y python3 openvpn python3-pyqt5 pkexec curl
-    else
-      echo "Unsupported Linux distribution."
-      exit 1
-    fi
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-    if command -v brew &> /dev/null; then
-      brew install python openvpn pyqt5 curl
-    else
-      echo "Homebrew is required on macOS. Please install it first."
-      exit 1
-    fi
-  else
-    echo "Unsupported OS."
-    exit 1
-  fi
+install_arch() {
+    sudo pacman -S --needed python openvpn python-pyqt5 curl
+    wget "${ARCHIVE_URL}" -O "${PKG_NAME}-${PKG_VER}.tar.gz"
+    tar xf "${PKG_NAME}-${PKG_VER}.tar.gz"
+    cd "${SRC_DIR}"
+
+    # Install the Python script
+    sudo install -Dm755 "src/main.py" "${INSTALL_DIR}/ovpnconn.py"
+    
+    # Create a shell script to execute ovpnconn.py and copy it to /usr/local/bin
+    echo '#!/bin/bash' | sudo tee "${INSTALL_DIR}/ovpnconn" > /dev/null
+    echo 'python3 /usr/local/bin/ovpnconn.py "$@"' | sudo tee -a "${INSTALL_DIR}/ovpnconn" > /dev/null
+    sudo chmod +x "${INSTALL_DIR}/ovpnconn"
+
+    # Install the icon
+    sudo install -Dm644 "src/ovpnconn-iconlogo.png" "${ICON_DIR}/ovpnconn.png"
+
+    # Install the .desktop file
+    sudo install -Dm644 "src/ovpnconn.desktop" "${DESKTOP_DIR}/ovpnconn.desktop"
 }
 
-# Function to download and extract the source code
-download_source() {
-  curl -L "${ARCHIVE_URL}" -o "v.${PKG_VER}.tar.gz"
-  tar xf "v.${PKG_VER}.tar.gz"
+install_debian() {
+    sudo apt-get update
+    sudo apt-get install -y python3 python3-pyqt5 openvpn curl
+    wget "${ARCHIVE_URL}" -O "${PKG_NAME}-${PKG_VER}.tar.gz"
+    tar xf "${PKG_NAME}-${PKG_VER}.tar.gz"
+    cd "${SRC_DIR}"
+
+    # Install the Python script
+    sudo install -Dm755 "src/main.py" "${INSTALL_DIR}/ovpnconn.py"
+    
+    # Create a shell script to execute ovpnconn.py and copy it to /usr/local/bin
+    echo '#!/bin/bash' | sudo tee "${INSTALL_DIR}/ovpnconn" > /dev/null
+    echo 'python3 /usr/local/bin/ovpnconn.py "$@"' | sudo tee -a "${INSTALL_DIR}/ovpnconn" > /dev/null
+    sudo chmod +x "${INSTALL_DIR}/ovpnconn"
+
+    # Install the icon
+    sudo install -Dm644 "src/ovpnconn-iconlogo.png" "${ICON_DIR}/ovpnconn.png"
+
+    # Install the .desktop file
+    sudo install -Dm644 "src/ovpnconn.desktop" "${DESKTOP_DIR}/ovpnconn.desktop"
 }
 
-# Function to install the package
-install_package() {
-  # Install the Python script
-  sudo install -Dm755 "${SRC_DIR}/src/main.py" "${INSTALL_DIR}/ovpnconn.py"
+install_macos() {
+    brew install python openvpn
+    wget "${ARCHIVE_URL}" -O "${PKG_NAME}-${PKG_VER}.tar.gz"
+    tar xf "${PKG_NAME}-${PKG_VER}.tar.gz"
+    cd "${SRC_DIR}"
 
-  # Create a shell script to execute ovpnconn.py
-  echo '#!/bin/bash' | sudo tee "${INSTALL_DIR}/ovpnconn" > /dev/null
-  echo "python3 ${INSTALL_DIR}/ovpnconn.py \"\$@\"" | sudo tee -a "${INSTALL_DIR}/ovpnconn" > /dev/null
-  sudo chmod +x "${INSTALL_DIR}/ovpnconn"
+    # Install the Python script
+    sudo install -m 755 "src/main.py" "${INSTALL_DIR}/ovpnconn.py"
+    
+    # Create a shell script to execute ovpnconn.py and copy it to /usr/local/bin
+    echo '#!/bin/bash' | sudo tee "${INSTALL_DIR}/ovpnconn" > /dev/null
+    echo 'python3 /usr/local/bin/ovpnconn.py "$@"' | sudo tee -a "${INSTALL_DIR}/ovpnconn" > /dev/null
+    sudo chmod +x "${INSTALL_DIR}/ovpnconn"
 
-  # Install the icon
-  sudo install -Dm644 -p "${SRC_DIR}/src/ovpnconn-iconlogo.png" "${ICON_DIR}/ovpnconn.png"
+    # Install the icon (use a directory that macOS supports)
+    sudo mkdir -p "${ICON_DIR}"
+    sudo cp "src/ovpnconn-iconlogo.png" "${ICON_DIR}/ovpnconn.png"
 
-  # Install the .desktop file (Linux only)
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    sudo install -Dm644 -p "${SRC_DIR}/src/ovpnconn.desktop" "${DESKTOP_DIR}/ovpnconn.desktop"
-  fi
+    # macOS doesn't use .desktop files, so this part is skipped
 }
 
-# Function to clean up
 cleanup() {
-  rm -rf "${SRC_DIR}" "v.${PKG_VER}.tar.gz"
+    echo "Cleaning up..."
+    cd ..
+    rm -rf "${PKG_NAME}-${PKG_VER}.tar.gz" "${SRC_DIR}"
+    echo "Cleanup completed."
 }
 
-# Execute functions
-welcome
-install_dependencies
-download_source
-install_package
-cleanup
+goodbye() {
+    echo "
+    ╔═══════════════════════════════════════╗
+    ║                                       ║
+    ║   ~ Installation Complete! ~          ║
+    ║   OpenVPN-GUI-Connect is now ready    ║
+    ║   to use. Enjoy a more secure         ║
+    ║   browsing experience!                ║
+    ║                                       ║
+    ║   Developed by Felipe Alfonso G.      ║
+    ║                                       ║
+    ╚═══════════════════════════════════════╝
+    "
+    echo "To launch OpenVPN-GUI-Connect, use the command: ovpnconn"
+}
 
-# Final message
-echo "
-╔═════════════════════════════════════════════════════════╗
-║                                                         ║
-║   ~ Installation Complete ~                             ║
-║   OpenVPN-GUI-Connect v${PKG_VER} has been successfully  ║
-║   installed on your system!                             ║
-║                                                         ║
-║   You can now launch the application by typing:         ║
-║   'ovpnconn' in your terminal.                          ║
-║                                                         ║
-║   Thank you for using OpenVPN-GUI-Connect!              ║
-╚═════════════════════════════════════════════════════════╝
-"
+main() {
+    welcome
+
+    case "$(uname)" in
+        Linux)
+            if command -v pacman &> /dev/null; then
+                install_arch
+            elif command -v apt-get &> /dev/null; then
+                install_debian
+            else
+                echo "Unsupported Linux distribution."
+                exit 1
+            fi
+            ;;
+        Darwin)
+            install_macos
+            ;;
+        *)
+            echo "Unsupported OS."
+            exit 1
+            ;;
+    esac
+
+    echo "Installation completed successfully!"
+    cleanup
+    goodbye
+}
+
+main
